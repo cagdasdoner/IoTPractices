86812b774a2f84c693b5e6093c85a19c3833e0ff
cagdasdoner <donercagdas@gmail.com>
2016-12-05 14:04:19 +0300

    Removed external HTTP library dependency.

--- src/device/esp8266_arduino/actuator/HTTPConnector.h
+++ src/device/esp8266_arduino/actuator/HTTPConnector.h
@@ -2,9 +2,8 @@
 #define ARDUINO_HTTPCONNECTOR_H
 
 #define LEN_HTTP_PATH_MAX 128
-
 #define HTTP_OK           200
 
-unsigned short HTTPPostCloud(char* data);
+boolean HTTPPostCloud(char* data);
 
 #endif /* ARDUINO_HTTPCONNECTOR_H */
--- src/device/esp8266_arduino/sensor_mote/HTTPConnector.cpp
+++ src/device/esp8266_arduino/sensor_mote/HTTPConnector.cpp
@@ -1,25 +1,36 @@
-#include <ESP8266WiFi.h>
-#include <ArduinoHttpClient.h>
+#include <ESP8266HTTPClient.h>
 
 #include "HTTPConnector.h"
 #include "Credentials.h"
 #include "Global.h"
 
-WiFiClient wifi;
-HttpClient thingSpeak = HttpClient(wifi, TSPEAK_HOST, TSPEAK_PORT);
-
-unsigned short HTTPPostCloud(char* data)
+unsigned short postThingSpeak(char* data)
 {
-  boolean retval = 0;
+  boolean httpCode = 0;
   char httpPath[LEN_HTTP_PATH_MAX];
   memset(httpPath, 0, LEN_HTTP_PATH_MAX);
-  snprintf(httpPath, LEN_HTTP_PATH_MAX, "/update?api_key=%s&field1=%s", TSPEAK_API_KEY, data);
+  snprintf(httpPath, LEN_HTTP_PATH_MAX, "%s/update?api_key=%s&field1=%s", TSPEAK_HOST, TSPEAK_API_KEY, data);
   Printf("path : %s\n", httpPath);
-  if(thingSpeak.get(httpPath) == 0)
+
+  HTTPClient http;
+  http.begin(httpPath);
+  /* HTTPS config. */
+  //http.begin(httpPath, "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38");
+  httpCode = http.GET();
+
+  Printf("Trace   : Return  : %d\n", httpCode);
+  Printf("Trace   : Incoming Body : %s\n", http.getString().c_str());
+  http.end();
+  
+  return httpCode;
+}
+
+boolean HTTPPostCloud(char* data)
+{
+  boolean retval = false;
+  if (postThingSpeak(data) == HTTP_OK)
   {
-    Printf("Trace   : Incoming Body : %d\n", thingSpeak.responseStatusCode());
-    Printf("Trace   : Incoming Body : %s\n", thingSpeak.responseBody().c_str());
-    retval = thingSpeak.responseStatusCode();
+    retval = true;
   }
   
   return retval;
